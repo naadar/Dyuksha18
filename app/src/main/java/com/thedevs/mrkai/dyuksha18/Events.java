edab6531e1d39c4cf2b8442965073792f39061cd
mrkai1253 <mrkai1253@gmail.com>
2018-10-12 22:32:03 +0530

    Font changed

--- app/src/main/java/com/thedevs/mrkai/dyuksha18/Events.java
+++ app/src/main/java/com/thedevs/mrkai/dyuksha18/Events.java
@@ -13,7 +13,7 @@ public class Events extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        return inflater.inflate(R.layout.frag_events, container, false);
     }
 
 
--- app/src/main/java/com/thedevs/mrkai/dyuksha18/Users.java
+++ app/src/main/java/com/thedevs/mrkai/dyuksha18/Users.java
@@ -13,7 +13,7 @@ public class Users extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        return inflater.inflate(R.layout.frag_user, container, false);
     }
 
 
